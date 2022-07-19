f6be99e8518916f20114de70544aad4ab2c1e59a
Samuel Mimram <smimram@gmail.com>
2022-07-19 16:22:10 +0200

    Better.

--- src/flows.ml
+++ src/flows.ml
@@ -8,9 +8,10 @@ let server =
   let callback _conn req body =
     let uri = req |> Request.uri in
     let path = Uri.path uri in
+    let query = Uri.query uri |> List.filter_map (fun (k,v) -> match v with [v] -> Some (k,v) | _ -> None) in
     let meth = req |> Request.meth in
     let headers = req |> Request.headers |> Header.to_list in
-
+    
     (* Answer an authorization request. *)
     let unauthorized () =
       let headers = Header.of_list ["WWW-Authenticate", Auth.string_of_challenge (`Basic "Please enter your credentials")] in
@@ -39,6 +40,8 @@ let server =
 
     let* body = Cohttp_lwt.Body.to_string body in
     Printf.printf "Serving %s for %s: %s\n%!" path (Code.string_of_method meth) body;
+
+    Printf.printf "  query: %s\n%!" (query |> List.map (fun (k,v) -> k ^ "=" ^ v) |> String.concat "&");
 
     match path with
     | _ -> Server.respond_string ~status:`OK ~body:(Printf.sprintf "This is the page for %s." path) ()
