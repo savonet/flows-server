80fc9e3ff456838f4276c1acbb8d273e6ec42f5b
Samuel Mimram <smimram@gmail.com>
2022-07-21 09:28:07 +0200

    Check GeoIP.

--- src/GeoIP.ml
+++ src/GeoIP.ml
@@ -1,5 +1,13 @@
+let dat = "GeoIPCity.dat"
+
+let () =
+  if Sys.command "geoiplookup -v localhost > /dev/null" <> 0 then
+    Printf.printf "WARNING: geoiplookup not installed, location will not work";
+  if not (Sys.file_exists dat) then
+    Printf.printf "WARNING: %s file not present for geoiplookup, location will not work" dat
+
 let lookup_string ip =
-  let cmd = Filename.quote_command "geoiplookup" ["-f"; "GeoIPCity.dat"; ip] in
+  let cmd = Filename.quote_command "geoiplookup" ["-f"; dat; ip] in
   let ic = Unix.open_process_in cmd in
   let s = input_line ic in
   ignore (Unix.close_process_in ic);
