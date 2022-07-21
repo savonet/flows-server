b1c17ef5440e3351c5208221a19bc4f8a47f90cd
Samuel Mimram <smimram@gmail.com>
2022-07-21 09:36:21 +0200

    Use a map.

--- src/DB.ml
+++ src/DB.ml
@@ -1,11 +1,13 @@
 (** Abstract database operations. *)
 
+module M = Map.Make(struct type t = string let compare (x:string) (y:string) = compare x y end)
+
 (** A database. *)
 type 'a t = {
   name : string;
   to_json : 'a -> JSON.t;
   of_json : JSON.t -> 'a;
-  mutable table : (string * 'a) list;
+  mutable table : 'a M.t;
   save_every : float; (** wait at least for this number of seconds before saving *)
   mutable save_last : float; (** when we last saved *)
   mutex_table : Mutex.t;
@@ -16,7 +18,7 @@ let filename db = db.name ^ ".db"
 
 (** Create a database. *)
 let create ~to_json ~of_json ?(every=0.) name =
-  let db = { name; to_json; of_json; table = []; save_every = every; save_last = 0.; mutex_table = Mutex.create (); mutex_file = Mutex.create () } in
+  let db = { name; to_json; of_json; table = M.empty; save_every = every; save_last = 0.; mutex_table = Mutex.create (); mutex_file = Mutex.create () } in
   let filename = filename db in
   if Sys.file_exists filename then (
     let ic = open_in filename in
@@ -25,7 +27,7 @@ let create ~to_json ~of_json ?(every=0.) name =
     let table =
       let table = JSON.of_string s in
       match table with
-        | `Assoc l -> List.map (fun (k, v) -> (k, of_json v)) l
+        | `Assoc l -> l |> List.to_seq |> Seq.map (fun (k, v) -> (k, of_json v)) |> M.of_seq
         | _ -> assert false
     in
     { db with table })
@@ -36,8 +38,7 @@ let create ~to_json ~of_json ?(every=0.) name =
 let add db k v =
   let table =
     Mutex.lock db.mutex_table;
-    let table = List.remove_assoc k db.table in
-    db.table <- (k, v) :: table;
+    db.table <- M.add k v db.table;
     db.table
   in
   Mutex.unlock db.mutex_table;
@@ -46,8 +47,7 @@ let add db k v =
   if now -. db.save_last >= db.save_every then
     (
       let oc = open_out (filename db) in
-      `Assoc (List.map (fun (k, v) -> (k, db.to_json v)) table)
-      |> JSON.to_string |> output_string oc;
+      table |> M.to_seq |> Seq.map (fun (k, v) -> (k, db.to_json v)) |> List.of_seq |> (fun l -> `Assoc l) |> JSON.to_string |> output_string oc;
       close_out oc;
       db.save_last <- now;
       Mutex.unlock db.mutex_file
@@ -56,7 +56,7 @@ let add db k v =
     Mutex.unlock db.mutex_file
 
 (** Find an entry. *)
-let find_opt db k = List.assoc_opt k db.table
+let find_opt db k = M.find_opt k db.table
 
 (** Iterator over database. *)
-let to_seq db = List.to_seq db.table
+let to_seq db = M.to_seq db.table
