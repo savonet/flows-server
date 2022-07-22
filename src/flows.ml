b6770b8121c55329e13c35e34391ecb8a3c156a9
Samuel Mimram <smimram@gmail.com>
2022-07-22 17:58:07 +0200

    Start map.

--- src/flows.ml
+++ src/flows.ml
@@ -167,6 +167,47 @@ function play(stream)
                     let streams = List.map (fun s -> Printf.sprintf "📻 <a href=\"%s\">%s</a>" s.Radio.url s.format) r.Radio.streams |> String.concat ", " in
                     Printf.sprintf "<a href=\"%s\">%s</a>: %s (%s)<br/>▶ <em>%s</em> by %s" r.Radio.website r.name r.description streams r.title r.artist
                  ) radios);
+            (* Map *)
+            HTML.h2 h "The map";
+            HTML.w h {|
+<link rel="stylesheet" href="https://cdn.rawgit.com/openlayers/openlayers.github.io/master/en/v5.3.0/css/ol.css" type="text/css">
+<script src="https://cdn.rawgit.com/openlayers/openlayers.github.io/master/en/v5.3.0/build/ol.js"></script>
+<div id="map" style="width: 600px; height: 100%%;"></div>
+<script>
+ var attribution = new ol.control.Attribution({
+     collapsible: false
+ });
+
+ var map = new ol.Map({
+     controls: ol.control.defaults({attribution: false}).extend([attribution]),
+     layers: [
+         new ol.layer.Tile({
+             source: new ol.source.OSM({
+                 url: 'https://tile.openstreetmap.be/osmbe/{z}/{x}/{y}.png',
+                 attributions: [ ol.source.OSM.ATTRIBUTION, 'Tiles courtesy of <a href="https://geo6.be/">GEO-6</a>' ],
+                 maxZoom: 18
+             })
+         })
+     ],
+     target: 'map',
+     view: new ol.View({
+         center: ol.proj.fromLonLat([4.35247, 50.84673]),
+         maxZoom: 18,
+         zoom: 12
+     })
+ });
+var layer = new ol.layer.Vector({
+     source: new ol.source.Vector({
+         features: [
+             new ol.Feature({
+                 geometry: new ol.geom.Point(ol.proj.fromLonLat([4.35247, 50.84673]))
+             })
+         ]
+     })
+ });
+ map.addLayer(layer);
+</script>
+|};
             Server.respond_string ~status:`OK ~body:(HTML.to_string h) ()
           | _ -> failwith "Invalid method."
         )
